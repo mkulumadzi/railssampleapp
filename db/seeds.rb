3902ba45e834a60fb908f3ba5bc4adfc4eb802c8
Evan Waters <evan.waters@gmail.com>
2016-02-03 15:15:12 -0800

    Updated seeds and fixtures with activation columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name: "Example User", email: "example@example.com", password: "password", password_confirmation: "password", admin: true)
+User.create!(name: "Example User", email: "example@example.com", password: "password", password_confirmation: "password", admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@example.com"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
